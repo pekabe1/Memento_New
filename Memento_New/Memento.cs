a0da2d0901e9abe6c21d88adfe31363ae2910048
k.ziarkowski <k.ziarkowski@PK3-3>
2019-10-29 20:04:50 +0100

    Add project files.

---
+++ Memento_New/Memento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento_New
+{
+    class Memento
+    {
+        string text { get; set; }
+        public void SetMemento(string text)
+        {
+            this.text = text;
+        }
+        public string GetMemento()
+        {
+            return text;
+        }
+    }
+}
---
+++ Memento_New/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Memento_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var m = new Memento();
+            Console.WriteLine("sample text");
+            m.SetMemento(Console.ReadLine());
+            Console.WriteLine(m.GetMemento());
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Type new text");
+            Console.ReadLine();
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("return previus value from memento");
+            Console.WriteLine(m.GetMemento());
+
+            
+
+        }
+    }
+}
